5e70cd006980dc4114d8ea229bda5803b436404d
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-03-07 18:16:55 +0530

    Update App.js

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -95,7 +95,7 @@ function App() {
             setCountry(event.target.value);
           }}
         />
-        <label>Designation:</label>
+        <label>Position:</label>
         <input
           type="text"
           onChange={(event) => {
@@ -122,7 +122,7 @@ function App() {
                 <h3>Age: {val.age}</h3>
                 <h3>Country: {val.country}</h3>
                 <h3>Position: {val.position}</h3>
-                <h3>Wage: {val.wage}</h3>
+                <h3>Salary: {val.wage}</h3>
               </div>
               <div>
                 <input
