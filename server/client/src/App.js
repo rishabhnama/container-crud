c8b0c341ee05a5519bd7096a5b409a267f5ba360
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-04-18 16:42:57 +0530

    Update App.js

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [employeeList, setEmployeeList] = useState([]);
 
   const addEmployee = () => {
-    Axios.post("http://localhost:8000/create", {
+    Axios.post("http://127.0.0.1:50679/create", {
       name: name,
       age: age,
       country: country,
@@ -35,13 +35,13 @@ function App() {
   };
 
   const getEmployees = () => {
-    Axios.get("http://localhost:8000/employees").then((response) => {
+    Axios.get("http://127.0.0.1:50679/employees").then((response) => {
       setEmployeeList(response.data);
     });
   };
 
   const updateEmployeeWage = (id) => {
-    Axios.put("http://localhost:8000/update", { wage: newWage, id: id }).then(
+    Axios.put("http://127.0.0.1:50679/update", { wage: newWage, id: id }).then(
       (response) => {
         setEmployeeList(
           employeeList.map((val) => {
@@ -62,7 +62,7 @@ function App() {
   };
 
   const deleteEmployee = (id) => {
-    Axios.delete(`http://localhost:8000/delete/${id}`).then((response) => {
+    Axios.delete(`http://127.0.0.1:50679/delete/${id}`).then((response) => {
       setEmployeeList(
         employeeList.filter((val) => {
           return val.id != id;
