822b7a07d6b8ca5a235de198a9771f5279585a30
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-04-18 11:36:02 +0530

    Update index.js

--- server/server/index.js
+++ server/server/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 // const cors = require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 
 // app.use(cors());
 app.use(express.json());
