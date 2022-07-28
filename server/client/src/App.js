99b1f965d5a09bbc709440e081923ca6d9b8b727
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-07-28 17:52:49 +0530

    Update App.js

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 
 function App() {
   console.log(`Name: ${process.env.REACT_APP_NAME}`);
+  console.log(`Name: ${process.env.REACT_APP_FIRSTNAME}`);
+  console.log(`Name: ${process.env.REACT_APP_LASTNAME}`);
+  console.log(`Name: ${process.env.REACT_APP_AGE}`);
+  console.log(`Name: ${process.env.REACT_APP_STATE}`);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Name: {process.env.REACT_APP_NAME}</p>
-        <p>Company: {process.env.REACT_APP_COMPANY}</p>
+        <p>Name: {process.env.REACT_APP_FIRSTNAME}</p>
+        <p>Name: {process.env.REACT_APP_LASTNAME}</p>
+        <p>Name: {process.env.REACT_APP_AGE}</p>
+        <p>Company: {process.env.REACT_APP_STATE}</p>
       </header>
     </div>
   );
