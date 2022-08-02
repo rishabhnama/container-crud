7662f172bf9139188bce8742814d2ce46914c164
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-08-02 16:46:31 +0530

    Update index.js

--- server/server/index.js
+++ server/server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 console.log(`Name: ${process.env.PROFILE_NAME}`);
-console.log(`Company: ${process.env.PROFILE_FIRSTNAME}`);
-console.log(`id: ${process.env.PROFILE_LASTNAME}`);
-console.log(`id: ${process.env.PROFILE_AGE}`);
-console.log(`id: ${process.env.PROFILE_STATE}`);
+console.log(`First Name: ${process.env.PROFILE_FIRSTNAME}`);
+console.log(`Last Name: ${process.env.PROFILE_LASTNAME}`);
+console.log(`Age: ${process.env.PROFILE_AGE}`);
+console.log(`State: ${process.env.PROFILE_STATE}`);
